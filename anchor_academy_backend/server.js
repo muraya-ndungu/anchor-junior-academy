5330bdaf6acdd82f3d0cd360590969d50898fcfc
muraya-ndungu <ndungu.muraya@s.karu.ac.ke>
2024-05-31 11:49:54 +0300

    mongo route

--- anchor_academy_backend/server.js
+++ anchor_academy_backend/server.js
@@ -5,37 +5,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-// Load environment variables
 dotenv.config();
 
-// Initialize app
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json()); // This should be before your routes
 
-// Connect to the database
-connectDB();
-
-// Routes
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth.js');
 app.use('/api/auth', authRoutes);
 
-// Root endpoint
-app.get('/', (req, res) => {
-    res.send('Anchorjr Academy Backend');
-});
-
-// Database connection and server start
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+connectDB();
+
+mongoose.connect(MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
@@ -43,3 +27,10 @@ mongoose.connect(MONGO_URI, {
 }).catch(err => {
     console.error(err.message);
 });
+
+app.get('/', (req, res) => {
+    res.send('Anchorjr Academy Backend');
+});
+
+// Middleware
+app.use(express.json());
